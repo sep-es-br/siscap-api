e52dacf8a568ae3c9c882b504261dfeb4765129c
oLuizGuilherme <luizgmz.lgmz@gmail.com>
2024-03-15 13:45:36 -0300

    fix: correção da ordem de inserts do script V20

--- src/main/resources/db/migration/V20__criar_campo_eixo_projetos.sql
+++ src/main/resources/db/migration/V20__criar_campo_eixo_projetos.sql
@@ -14,12 +14,13 @@ alter table projeto add column id_eixo integer;
 alter table projeto
     add constraint fk_projeto_id_eixo_eixo foreign key (id_eixo) references eixo (id);
 
+insert into eixo (id, nome, id_plano, criado_em) values (1, 'Eixo 1: +Qualidade de vida', 1, '2024-03-13 13:32:02.780759');
+insert into eixo (id, nome, id_plano, criado_em) values (2, 'Eixo 2: +Des. com sustentabilidade', 1, '2024-03-13 13:32:02.780759');
+insert into eixo (id, nome, id_plano, criado_em) values (3, 'Eixo 3: +Resultados', 1, '2024-03-13 13:32:02.780759');
+
 update projeto set id_plano = 1 where id_plano is null;
 update projeto set id_eixo = 1 where id_eixo is null;
 
 alter table projeto alter column id_plano set not null;
-alter table projeto alter column id_eixo set not null;
 
-insert into eixo (id, nome, id_plano, criado_em) values (1, 'Eixo 1: +Qualidade de vida', 1, '2024-03-13 13:32:02.780759');
-insert into eixo (id, nome, id_plano, criado_em) values (2, 'Eixo 2: +Des. com sustentabilidade', 1, '2024-03-13 13:32:02.780759');
-insert into eixo (id, nome, id_plano, criado_em) values (3, 'Eixo 3: +Resultados', 1, '2024-03-13 13:32:02.780759');
+alter table projeto alter column id_eixo set not null;
