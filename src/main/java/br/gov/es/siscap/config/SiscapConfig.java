0e275bf4c4ad849e15c962b45d59fa9643753517
oLuizGuilherme <luizgmz.lgmz@gmail.com>
2024-06-04 09:31:01 -0300

    fix: fix the time zone of the application

--- src/main/java/br/gov/es/siscap/config/SiscapConfig.java
+++ src/main/java/br/gov/es/siscap/config/SiscapConfig.java
@@ -2,12 +2,14 @@ package br.gov.es.siscap.config;
 
 import br.gov.es.siscap.infra.Roles;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.TimeZone;
 
 @Configuration
 public class SiscapConfig {
@@ -21,6 +23,11 @@ public class SiscapConfig {
     @Bean
     Roles roles() throws IOException {
         return new ObjectMapper().readValue(new File(authoritiesFile), Roles.class);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT-3"));
     }
 
 }
