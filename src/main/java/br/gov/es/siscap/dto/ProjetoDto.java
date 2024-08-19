ad7ee065e0081128051682f7372674b842d4a8d9
Artur Faria <artur.faria.1992@gmail.com>
2024-08-19 11:05:35 -0300

    ref: altera logica da tabela associativa entre Projeto, Pessoa e ProjetoPessoa

--- src/main/java/br/gov/es/siscap/dto/ProjetoDto.java
+++ src/main/java/br/gov/es/siscap/dto/ProjetoDto.java
@@ -1,12 +1,13 @@
 package br.gov.es.siscap.dto;
 
-import br.gov.es.siscap.models.Microrregiao;
-import br.gov.es.siscap.models.Pessoa;
-import br.gov.es.siscap.models.Projeto;
+import br.gov.es.siscap.models.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public record ProjetoDto(
 			Long id,
@@ -21,18 +22,84 @@ public record ProjetoDto(
 			String solucoesPropostas,
 			String impactos,
 			String arranjosInstitucionais,
-			List<Long> idMicrorregioes,
+//			List<Long> idMicrorregioes,
+			List<RateioDto> rateio,
 			Long idResponsavelProponente,
 			List<EquipeDto> equipeElaboracao) {
 
-	public ProjetoDto(Projeto projeto) {
-		this(projeto.getId(), projeto.getSigla(), projeto.getTitulo(), projeto.getValorEstimado(),
-					projeto.getObjetivo(), projeto.getObjetivoEspecifico(), projeto.getStatus().getId(),
-					projeto.getOrganizacao().getId(), projeto.getSituacaoProblema(), projeto.getSolucoesPropostas(),
-					projeto.getImpactos(), projeto.getArranjosInstitucionais(),
-					projeto.getMicrorregioes().stream().map(Microrregiao::getId).toList(),
-					projeto.getResponsavelProponente(), projeto.getEquipeElaboracao());
+//	public ProjetoDto(Projeto projeto) {
+//		this(projeto.getId(), projeto.getSigla(), projeto.getTitulo(), projeto.getValorEstimado(),
+//					projeto.getObjetivo(), projeto.getObjetivoEspecifico(), projeto.getStatus().getId(),
+//					projeto.getOrganizacao().getId(), projeto.getSituacaoProblema(), projeto.getSolucoesPropostas(),
+//					projeto.getImpactos(), projeto.getArranjosInstitucionais(),
+//					null, List.of());
+//					projeto.getMicrorregioes().stream().map(Microrregiao::getId).toList(),
+//					projeto.getResponsavelProponente(), projeto.getEquipeElaboracao());
+//	}
+
+//	public static ProjetoDto comProjetoPessoa(Projeto projeto, Set<ProjetoPessoa> projetoPessoaSet) {
+//		Long idResponsavelProponente = projetoPessoaSet.stream()
+//					.filter(ProjetoPessoa::isResponsavelProponente)
+//					.findFirst()
+//					.map(ProjetoPessoa::getPessoa)
+//					.map(Pessoa::getId)
+//					.orElse(null);
+//
+//		List<EquipeDto> equipeElaboracao = projetoPessoaSet.stream()
+//					.filter(Predicate.not(ProjetoPessoa::isResponsavelProponente))
+//					.map(EquipeDto::new)
+//					.collect(Collectors.toList());
+//
+//		return new ProjetoDto(projeto.getId(), projeto.getSigla(), projeto.getTitulo(), projeto.getValorEstimado(),
+//					projeto.getObjetivo(), projeto.getObjetivoEspecifico(), projeto.getStatus().getId(),
+//					projeto.getOrganizacao().getId(), projeto.getSituacaoProblema(), projeto.getSolucoesPropostas(),
+//					projeto.getImpactos(), projeto.getArranjosInstitucionais(),
+//					idResponsavelProponente, equipeElaboracao);
+//	}
+
+	public static ProjetoDto montar(Projeto projeto, Set<ProjetoPessoa> projetoPessoaSet, Set<ProjetoCidade> projetoCidadeSet) {
+		Long idResponsavelProponente = projetoPessoaSet.stream()
+					.filter(ProjetoPessoa::isResponsavelProponente)
+					.findFirst()
+					.map(ProjetoPessoa::getPessoa)
+					.map(Pessoa::getId)
+					.orElse(null);
+
+		List<EquipeDto> equipeElaboracao = projetoPessoaSet.stream()
+					.filter(Predicate.not(ProjetoPessoa::isResponsavelProponente))
+					.map(EquipeDto::new)
+					.toList();
+
+		List<RateioDto> rateio = projetoCidadeSet.stream()
+					.map(RateioDto::new)
+					.toList();
+
+		return new ProjetoDto(
+					projeto.getId(),
+					projeto.getSigla(),
+					projeto.getTitulo(),
+					projeto.getValorEstimado(),
+					projeto.getObjetivo(),
+					projeto.getObjetivoEspecifico(),
+					projeto.getStatus().getId(),
+					projeto.getOrganizacao().getId(),
+					projeto.getSituacaoProblema(),
+					projeto.getSolucoesPropostas(),
+					projeto.getImpactos(),
+					projeto.getArranjosInstitucionais(),
+					rateio,
+					idResponsavelProponente,
+					equipeElaboracao);
 	}
 
-
+//	public ProjetoDto(Projeto projeto, Set<ProjetoPessoa> projetoPessoaSet) {
+//		this(projeto.getId(), projeto.getSigla(), projeto.getTitulo(), projeto.getValorEstimado(),
+//					projeto.getObjetivo(), projeto.getObjetivoEspecifico(), projeto.getStatus().getId(),
+//					projeto.getOrganizacao().getId(), projeto.getSituacaoProblema(), projeto.getSolucoesPropostas(),
+//					projeto.getImpactos(), projeto.getArranjosInstitucionais(),
+////					projeto.getMicrorregioes().stream().map(Microrregiao::getId).toList(),
+//					projetoPessoaSet.stream().filter(projetoPessoa -> projetoPessoa.getPapel().getId() == 2L)
+//								.findFirst().map(projetoPessoa -> projetoPessoa.getPessoa().getId()).orElse(null),
+//					projetoPessoaSet.stream().filter(projetoPessoa -> projetoPessoa.getPapel().getId() != 2L).map(EquipeDto::new).toList());
+//	}
 }
--- src/main/java/br/gov/es/siscap/service/ProjetoService.java
+++ src/main/java/br/gov/es/siscap/service/ProjetoService.java
@@ -2,10 +2,13 @@ package br.gov.es.siscap.service;
 
 import br.gov.es.siscap.dto.ProjetoDto;
 import br.gov.es.siscap.dto.listagem.ProjetoListaDto;
+import br.gov.es.siscap.exception.RelatorioNomeArquivoException;
 import br.gov.es.siscap.exception.ValidacaoSiscapException;
 import br.gov.es.siscap.exception.naoencontrado.ProjetoNaoEncontradoException;
 import br.gov.es.siscap.form.ProjetoForm;
 import br.gov.es.siscap.models.Projeto;
+import br.gov.es.siscap.models.ProjetoCidade;
+import br.gov.es.siscap.models.ProjetoPessoa;
 import br.gov.es.siscap.repository.ProjetoRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -16,102 +19,128 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ProjetoService {
 
-    private final ProjetoRepository repository;
-    private final OrganizacaoService organizacaoService;
-    private final MicrorregiaoService microrregiaoService;
-    private final Logger logger = LogManager.getLogger(ProjetoService.class);
+	private final ProjetoRepository repository;
+	private final ProjetoPessoaService projetoPessoaService;
+	private final ProjetoCidadeService projetoCidadeService;
+	private final OrganizacaoService organizacaoService;
+	private final Logger logger = LogManager.getLogger(ProjetoService.class);
 
-    @Transactional
-    public ProjetoDto salvar(ProjetoForm form) {
-        logger.info("Cadatrar novo projeto: {}.", form);
-        validarProjeto(form, true);
-        Projeto projeto = repository.save(new Projeto(form));
-        logger.info("Cadastro de projeto finalizado!");
-        return new ProjetoDto(projeto);
-    }
+	@Transactional
+	public ProjetoDto salvar(ProjetoForm form) {
+		logger.info("Cadastrar novo projeto: {}.", form);
+		validarProjeto(form, true);
+		Projeto projeto = repository.save(new Projeto(form));
 
-    public Page<ProjetoListaDto> listarTodos(Pageable pageable) {
-        return repository.findAll(pageable).map(ProjetoListaDto::new);
-    }
+		Set<ProjetoPessoa> projetoPessoaSet = projetoPessoaService.salvar(projeto, form.idResponsavelProponente(), form.equipeElaboracao());
 
-    @Transactional
-    public ProjetoDto atualizar(Long id, ProjetoForm form) {
-        logger.info("Atualizar projeto de id {}: {}.", id, form);
-        validarProjeto(form, false);
-        Projeto projeto = buscarPorId(id);
-        projeto.atualizarProjeto(form);
-        repository.save(projeto);
-        return new ProjetoDto(projeto);
-    }
+		Set<ProjetoCidade> projetoCidadeSet = projetoCidadeService.salvar(projeto, form.rateio());
 
-    @Transactional
-    public void excluir(Long id) {
-        logger.info("Excluir projeto {}.", id);
-        Projeto projeto = buscarPorId(id);
-        projeto.apagar();
-        repository.saveAndFlush(projeto);
-        repository.deleteById(id);
-        logger.info("Exclusão do projeto com id {} finalizada!", id);
-    }
+		logger.info("Cadastro de projeto finalizado!");
+		return ProjetoDto.montar(projeto, projetoPessoaSet, projetoCidadeSet);
+	}
 
-    public ProjetoDto buscar(Long id) {
-        return new ProjetoDto(buscarPorId(id));
-    }
+	public Page<ProjetoListaDto> listarTodos(Pageable pageable) {
+		return repository
+					.findAll(pageable)
+					.map(projeto -> new ProjetoListaDto(projeto, projetoCidadeService.listarNomesCidadesPorProjeto(projeto)));
+	}
 
-    public String gerarNomeArquivo(Integer idProjeto) {
-        Projeto projeto = buscarPorId(Long.valueOf(idProjeto));
+	@Transactional
+	public ProjetoDto atualizar(Long id, ProjetoForm form) {
+		logger.info("Atualizar projeto de id {}: {}.", id, form);
+		validarProjeto(form, false);
+		Projeto projeto = buscarPorId(id);
+		projeto.atualizarProjeto(form);
+		Projeto projetoResult = repository.save(projeto);
 
-        String cnpj = formatarCnpj(projeto.getOrganizacao().getCnpj());
+		Set<ProjetoPessoa> projetoPessoaSet = projetoPessoaService.atualizar(projetoResult, form.idResponsavelProponente(), form.equipeElaboracao());
 
-        return "PROJETO n. " +
-                projeto.getId() + "/" +
-                projeto.getCriadoEm().getYear() + "-" +
-                projeto.getOrganizacao().getNomeFantasia() + "-" +
-                cnpj;
-    }
+		Set<ProjetoCidade> projetoCidadeSet = projetoCidadeService.atualizar(projetoResult, form.rateio());
 
-    public int buscarQuantidadeProjetos() {
-        return Integer.parseInt(String.valueOf((repository.count())));
-    }
+		logger.info("Atualização do projeto de id: {} finalizada!", projetoResult.getId());
+		return ProjetoDto.montar(projeto, projetoPessoaSet, projetoCidadeSet);
+	}
 
-    public BigDecimal buscarSomatorioValorEstimadoProjetos() {
-        return repository.somarValorEstimadoTodosProjetos();
-    }
+	@Transactional
+	public void excluir(Long id) {
+		logger.info("Excluir projeto {}.", id);
+		Projeto projeto = buscarPorId(id);
+		projeto.apagar();
+		repository.saveAndFlush(projeto);
+		repository.deleteById(id);
 
-    private Projeto buscarPorId(Long id) {
-        return repository.findById(id).orElseThrow(() -> new ProjetoNaoEncontradoException(id));
-    }
+		projetoPessoaService.excluirPorProjeto(projeto);
+		projetoCidadeService.excluir(projeto);
 
-    private void validarProjeto(ProjetoForm form, boolean isSalvar) {
-        List<String> erros = new ArrayList<>();
+		logger.info("Exclusão do projeto com id {} finalizada!", id);
+	}
 
-        if (!organizacaoService.existePorId(form.idOrganizacao())) {
-            erros.add("Erro ao encontrar Organização com id " + form.idOrganizacao());
-        }
-        form.idMicrorregioes().forEach(id -> {
-            if (!microrregiaoService.existePorId(id))
-                erros.add("Erro ao encontrar Microrregião com id " + id);
-        });
+	public ProjetoDto buscar(Long id) {
+		Projeto projeto = this.buscarPorId(id);
 
-        if (repository.existsBySigla(form.sigla()) && isSalvar)
-            erros.add("Já existe um projeto cadastrado com essa sigla.");
+		Set<ProjetoPessoa> projetoPessoaSet = projetoPessoaService.buscarPorProjeto(projeto);
 
-        if (!erros.isEmpty()) {
-            erros.forEach(logger::error);
-            throw new ValidacaoSiscapException(erros);
-        }
-    }
+		Set<ProjetoCidade> projetoCidadeSet = projetoCidadeService.buscarPorProjeto(projeto);
 
-    private String formatarCnpj(String cnpj) {
-        return cnpj.replaceAll("^(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})$", "$1.$2.$3/$4-$5");
-    }
+		return ProjetoDto.montar(projeto, projetoPessoaSet, projetoCidadeSet);
+	}
+
+	public String gerarNomeArquivo(Integer idProjeto) {
+		Projeto projeto = buscarPorId(Long.valueOf(idProjeto));
+
+		if(projeto.getOrganizacao().getCnpj() == null) {
+			throw new RelatorioNomeArquivoException("Organização não possui CNPJ.");
+		}
+
+		String cnpj = formatarCnpj(projeto.getOrganizacao().getCnpj());
+
+		return "PROJETO n. " +
+					projeto.getId() + "/" +
+					projeto.getCriadoEm().getYear() + "-" +
+					projeto.getOrganizacao().getNomeFantasia() + "-" +
+					cnpj;
+	}
+
+	public int buscarQuantidadeProjetos() {
+		return Integer.parseInt(String.valueOf((repository.count())));
+	}
+
+	public BigDecimal buscarSomatorioValorEstimadoProjetos() {
+		return repository.somarValorEstimadoTodosProjetos();
+	}
+
+	private Projeto buscarPorId(Long id) {
+		return repository.findById(id).orElseThrow(() -> new ProjetoNaoEncontradoException(id));
+	}
+
+	private void validarProjeto(ProjetoForm form, boolean isSalvar) {
+		List<String> erros = new ArrayList<>();
+
+		if (!organizacaoService.existePorId(form.idOrganizacao())) {
+			erros.add("Erro ao encontrar Organização com id " + form.idOrganizacao());
+		}
+
+		if (repository.existsBySigla(form.sigla()) && isSalvar)
+			erros.add("Já existe um projeto cadastrado com essa sigla.");
+
+		if (!erros.isEmpty()) {
+			erros.forEach(logger::error);
+			throw new ValidacaoSiscapException(erros);
+		}
+	}
+
+	private String formatarCnpj(String cnpj) {
+		return cnpj.replaceAll("^(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})$", "$1.$2.$3/$4-$5");
+	}
 
 }
