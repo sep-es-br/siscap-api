ea4afc3ceb76a55dd06ae6058a39cf7d82239f04
Artur Faria <artur.faria.1992@gmail.com>
2024-11-27 18:45:38 -0300

    ref: corrige comportamento de tratamento de erro de token

--- src/main/java/br/gov/es/siscap/service/TokenService.java
+++ src/main/java/br/gov/es/siscap/service/TokenService.java
@@ -1,7 +1,9 @@
 package br.gov.es.siscap.service;
 
+import br.gov.es.siscap.enums.FormatoDataEnum;
 import br.gov.es.siscap.exception.service.SiscapServiceException;
 import br.gov.es.siscap.models.Usuario;
+import br.gov.es.siscap.utils.FormatadorData;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
@@ -16,35 +18,40 @@ import java.util.List;
 @Service
 public class TokenService {
 
-    private static final String ISSUER = "SEP Siscap API";
+	private static final String ISSUER = "SEP Siscap API";
 
-    @Value("${token.secret}")
-    private String secret;
+	@Value("${token.secret}")
+	private String secret;
 
-    public String gerarToken(Usuario usuario) {
-        try {
-            Algorithm algoritmo = Algorithm.HMAC256(secret);
-            return JWT.create()
-                    .withIssuer(ISSUER)
-                    .withSubject(usuario.getSub())
-                    .withExpiresAt(getDataExpiracao())
-                    .sign(algoritmo);
-        } catch (JWTCreationException exception) {
-            throw new SiscapServiceException(List.of("Erro ao gerar o token", exception.getMessage()));
-        }
-    }
+	public String gerarToken(Usuario usuario) {
+		try {
+			Algorithm algoritmo = Algorithm.HMAC256(secret);
+			return JWT.create()
+						.withIssuer(ISSUER)
+						.withSubject(usuario.getSub())
+						.withExpiresAt(getDataExpiracao())
+						.sign(algoritmo);
+		} catch (JWTCreationException exception) {
+			throw new SiscapServiceException(List.of("Erro ao gerar o token", exception.getMessage()));
+		}
+	}
 
-    public String validarToken(String token) {
-        Algorithm algoritmo = Algorithm.HMAC256(secret);
-        return JWT.require(algoritmo)
-                .withIssuer(ISSUER)
-                .build()
-                .verify(token)
-                .getSubject();
-    }
+	public String validarToken(String token) {
+		Algorithm algoritmo = Algorithm.HMAC256(secret);
+		return JWT.require(algoritmo)
+					.withIssuer(ISSUER)
+					.build()
+					.verify(token)
+					.getSubject();
+	}
 
-    private Instant getDataExpiracao() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-    }
+	public String buscarDataExpiracaoToken(String token) {
+		LocalDateTime dataExpiracao = LocalDateTime.ofInstant(JWT.decode(token).getExpiresAt().toInstant(), ZoneOffset.of("-03:00"));
+		return FormatadorData.formatar(dataExpiracao, FormatoDataEnum.COMPLETO);
+	}
+
+	private Instant getDataExpiracao() {
+		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+	}
 
 }
