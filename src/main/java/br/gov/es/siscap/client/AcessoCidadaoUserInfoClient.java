3fa9377e39978c6fe61f587114648259e651990a
Artur Faria <artur.faria.1992@gmail.com>
2024-12-18 09:45:26 -0300

    fix: altera retorno do metodo buscarUserInfoAcessoCidadao para tratar caso propriedade 'role' como null, String ou List<String>

--- src/main/java/br/gov/es/siscap/client/AcessoCidadaoUserInfoClient.java
+++ src/main/java/br/gov/es/siscap/client/AcessoCidadaoUserInfoClient.java
@@ -1,15 +1,15 @@
 package br.gov.es.siscap.client;
 
-import br.gov.es.siscap.dto.acessocidadaoapi.ACUserInfoDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @FeignClient(name = "acessoCidadaoUserInfo", url = "${api.acessocidadao.uri.userinfo}")
 public interface AcessoCidadaoUserInfoClient {
 
 	@GetMapping
-	ACUserInfoDto.ACUserInfoDtoStringRole buscarUserInfoAcessoCidadao(@RequestHeader Map<String, Object> headers);
+	LinkedHashMap<String, Object> buscarUserInfoAcessoCidadao(@RequestHeader Map<String, Object> headers);
 }
