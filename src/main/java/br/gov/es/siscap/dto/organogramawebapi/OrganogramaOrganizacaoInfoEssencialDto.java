caf9e78afaa3ebd9de508eb6d770939fde93ba27
Artur Faria <artur.faria.1992@gmail.com>
2025-01-31 19:01:01 -0300

    feat: cria dto OrganogramaOrganizacaoInfoEssencialDto

---
+++ src/main/java/br/gov/es/siscap/dto/organogramawebapi/OrganogramaOrganizacaoInfoEssencialDto.java
@@ -0,0 +1,12 @@
+package br.gov.es.siscap.dto.organogramawebapi;
+
+public record OrganogramaOrganizacaoInfoEssencialDto(
+
+			String guid,
+			String cnpj,
+			String sigla,
+			String razaoSocial,
+			String nomeFantasia
+
+) {
+}
