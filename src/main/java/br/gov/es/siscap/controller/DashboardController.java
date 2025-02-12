5050c010bd6744258faa08bc410dd63578849744
Artur Faria <artur.faria.1992@gmail.com>
2025-02-12 16:20:58 -0300

    ref: adiciona logica para buscar quantidade de Programas e CartasConsulta

--- src/main/java/br/gov/es/siscap/controller/DashboardController.java
+++ src/main/java/br/gov/es/siscap/controller/DashboardController.java
@@ -1,12 +1,15 @@
 package br.gov.es.siscap.controller;
 
-import br.gov.es.siscap.dto.DashboardProjetoDto;
+import br.gov.es.siscap.dto.DashboardDadosDto;
 import br.gov.es.siscap.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// 12/02/2025
+// ALTERACOES PROVISORIAS APENAS PARA APRESENTACAO; A SEREM REMOVIDAS POSTERIORMENTE
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,7 @@ public class DashboardController {
 	private final DashboardService service;
 
 	@GetMapping
-	public ResponseEntity<DashboardProjetoDto> quantidadeProjetos() {
-		return ResponseEntity.ok(service.buscarDashboardProjetos());
+	public ResponseEntity<DashboardDadosDto> buscarDadosDashboard() {
+		return ResponseEntity.ok(service.buscarDadosDashboard());
 	}
 }
