16d00c00293272ca2a2ee46d0d48fad70052e7ed
Gean Guimarães Carneiro <gean.carneiro@sep.local>
2025-03-07 17:07:43 -0300

    evolução

--- src/main/java/br/gov/es/siscap/models/Pessoa.java
+++ src/main/java/br/gov/es/siscap/models/Pessoa.java
@@ -1,17 +1,29 @@
 package br.gov.es.siscap.models;
 
-import br.gov.es.siscap.form.EnderecoForm;
-import br.gov.es.siscap.form.PessoaForm;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLJoinTableRestriction;
 import org.hibernate.annotations.SQLRestriction;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import br.gov.es.siscap.form.PessoaForm;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
