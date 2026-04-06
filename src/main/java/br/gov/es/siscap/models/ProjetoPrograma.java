b3f2eb2102122fecb8abafe45fcc0403c145ab05
geanCarneiro <geancarneiro1@gmail.com>
2026-04-06 10:22:41 -0300

    ajustes

--- src/main/java/br/gov/es/siscap/models/ProjetoPrograma.java
+++ src/main/java/br/gov/es/siscap/models/ProjetoPrograma.java
@@ -14,6 +14,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -23,6 +24,7 @@ import org.springframework.format.annotation.DateTimeFormat;
  */
 @Entity
 @Table(name = "projeto_programa")
+@NoArgsConstructor
 @Getter
 public class ProjetoPrograma {
     
