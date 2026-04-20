c9259ee91206585cac937029a0fa4cefcb2e997a
Eduardo Simonato <ersimonato.es@gmail.com>
2026-04-20 11:40:51 -0300

    commit ajustes indicadorexterno controller

--- src/main/java/br/gov/es/siscap/controller/IndicadorCatalogoController.java
+++ src/main/java/br/gov/es/siscap/controller/IndicadorCatalogoController.java
@@ -1,20 +1,10 @@
 package br.gov.es.siscap.controller;
 
-import br.gov.es.siscap.dto.OrganizacaoDto;
 import br.gov.es.siscap.dto.opcoes.OpcoesDto;
-import br.gov.es.siscap.dto.listagem.OrganizacaoListaDto;
-import br.gov.es.siscap.form.OrganizacaoForm;
-import br.gov.es.siscap.service.OrganizacaoService;
-import jakarta.validation.Valid;
+import br.gov.es.siscap.service.IndicadorExternoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,7 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class IndicadorCatalogoController {
 
-	private final OrganizacaoService service;
+	private final IndicadorExternoService service;
 
 	@GetMapping("/opcoes")
 	public List<OpcoesDto> listarOpcoesDropdown(
