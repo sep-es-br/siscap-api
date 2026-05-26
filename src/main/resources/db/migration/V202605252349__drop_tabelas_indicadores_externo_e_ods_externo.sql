bbd21f82b2239935a4235e8518b2cceeb54339b3
Eduardo Simonato <ersimonato.es@gmail.com>
2026-05-26 11:39:46 -0300

    atualizar flyway de exclusao das tabelas de indicadores e ods vindos do BI;

--- src/main/resources/db/migration/V202605252349__drop_tabelas_indicadores_externo_e_ods_externo.sql
+++ src/main/resources/db/migration/V202605252349__drop_tabelas_indicadores_externo_e_ods_externo.sql
@@ -1,21 +1,11 @@
 -- ============================================
--- REMOVER CONSTRAINTS
--- ============================================
-ALTER TABLE indicador_fato_externo
-DROP CONSTRAINT IF EXISTS fk_fato_indicador;
-
-ALTER TABLE projeto_indicador
-DROP CONSTRAINT IF EXISTS fk_projeto_indicador_indicador_externo;
-
-ALTER TABLE ods_indicador_externo
-DROP CONSTRAINT IF EXISTS fk_ods_indicador_externo_indicador;
-
--- ============================================
 -- REMOVER TABELAS
 -- Ordem importante por causa das dependências
 -- ============================================
-DROP TABLE IF EXISTS indicador_fato_externo;
+DROP TABLE IF EXISTS projeto_indicador_ods;
 
 DROP TABLE IF EXISTS ods_indicador_externo;
+
+DROP TABLE IF EXISTS indicador_fato_externo;
 
 DROP TABLE IF EXISTS indicador_externo;
