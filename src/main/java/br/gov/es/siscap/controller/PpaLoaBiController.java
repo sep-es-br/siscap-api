6bee9ca75ae33fe8bfd5d2304ece4cf159d5a93a
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2026-08-03 22:58:42 -0300

    ajustes controller ppaLoa novos endpoints

--- src/main/java/br/gov/es/siscap/controller/PpaLoaBiController.java
+++ src/main/java/br/gov/es/siscap/controller/PpaLoaBiController.java
@@ -3,6 +3,9 @@ package br.gov.es.siscap.controller;
 import br.gov.es.siscap.dto.indicadoresexternos.OpcoesPeriodoPpaLoaDto;
 import br.gov.es.siscap.dto.indicadoresexternos.OpcoesPpaLoaDto;
 import br.gov.es.siscap.service.PpaLoaBiService;
+import feign.Param;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.*;
@@ -21,20 +24,55 @@ public class PpaLoaBiController {
 		return service.listarPeriodoPpaAtivo();
 	}
 
-	@GetMapping("/ppa")
+	@GetMapping("/ppa/anos")
 	public List<OpcoesPpaLoaDto> listarAnosPeriodoPpaAtivo() {
-		return service.AnosPpaAtivo();
+		return service.listarAnosPpaAtivo();
 	}
 
+	@GetMapping("/ppa/uos/{ano}")
+	public List<OpcoesPpaLoaDto> listarUosPorAno(@PathVariable @NotNull Long ano) {
+		return service.listarUosAnoPpa(ano);
+	}
+
+	@GetMapping("/ppa/funcoes")
+	public List<OpcoesPpaLoaDto> listarFuncoes(
+        @RequestParam List<Long> anos,
+        @RequestParam List<Long> uos) {
+		return service.listarFuncoes( anos, uos );
+	}
+
+	@GetMapping("/ppa/programas")
+	public List<OpcoesPpaLoaDto> listarProgramas(
+        @RequestParam List<Long> anos,
+        @RequestParam List<Long> uos,
+		@RequestParam List<Long> funcoes ) {
+		return service.listarProgramas( anos, uos, funcoes );
+	}
+
+	@GetMapping("/ppa/acoes")
+	public List<OpcoesPpaLoaDto> listarAcoes(
+		@RequestParam List<Long> funcoes,
+		@RequestParam List<Long> programas,
+		@RequestParam List<Long> anos,
+		@RequestParam List<Long> uos) {
+		return service.listarAcoes( funcoes, programas, anos, uos );
+	}
+
+	// .set('funcoes', idFuncoes.join(','))
+    //   .set('programas', idsProgramas.join(','))
+    //   .set('anos', idAnos.join(','))
+    //   .set('uos', idUos.join(','));
+
 	// @PostMapping("/gestoes/{idGestao}/indicadores")
-	// public List<OpcoesIndicadoresDto> listarIndicadoresFiltro(@PathVariable @NotNull Long idGestao,
-	// 	@RequestBody(required = false) FiltroIndicadorDto filtro) {
-	// 	return service.listarIndicadoresFiltro(idGestao, filtro);
+	// public List<OpcoesIndicadoresDto> listarIndicadoresFiltro(@PathVariable
+	// @NotNull Long idGestao,
+	// @RequestBody(required = false) FiltroIndicadorDto filtro) {
+	// return service.listarIndicadoresFiltro(idGestao, filtro);
 	// }
 
 	// @GetMapping("/ods")
 	// public List<OdsPentahoBiDto> listarOds() {
-	// 	return service.listarOdsBI();
+	// return service.listarOdsBI();
 	// }
 
 }
--- src/main/java/br/gov/es/siscap/service/PpaLoaBiService.java
+++ src/main/java/br/gov/es/siscap/service/PpaLoaBiService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
@@ -34,6 +36,36 @@ public class PpaLoaBiService {
 	@Value("${pentahoBI.sigefes.planejamento.target}")
 	private String targetPlanejamentoPpa;
 
+	@Value("${pentahoBI.sigefes.anosppa.dataAccessId}")
+	private String anosPpaDataAccessId;
+
+	@Value("${pentahoBI.sigefes.anosppa.target}")
+	private String targetAnosPpa;
+
+	@Value("${pentahoBI.sigefes.uosano.dataAccessId}")
+	private String uosPpaDataAccessId;
+
+	@Value("${pentahoBI.sigefes.uosano.target}")
+	private String targetUosPpa;
+
+	@Value("${pentahoBI.sigefes.funcoes.dataAccessId}")
+	private String funcoesPpaDataAccessId;
+
+	@Value("${pentahoBI.sigefes.funcoes.target}")
+	private String targetFuncoesPpa;
+
+	@Value("${pentahoBI.sigefes.programas.dataAccessId}")
+	private String programasPpaDataAccessId;
+
+	@Value("${pentahoBI.sigefes.programas.target}")
+	private String targetProgramasPpa;
+
+	@Value("${pentahoBI.sigefes.acoes.dataAccessId}")
+	private String acoesPpaDataAccessId;
+
+	@Value("${pentahoBI.sigefes.acoes.target}")
+	private String targetAcoesPpa;
+
 	private final ApiUtils apiUtils;
 
 	public OpcoesPeriodoPpaLoaDto listarPeriodoPpaAtivo() {
@@ -46,424 +78,129 @@ public class PpaLoaBiService {
 
 		return apiUtils.consult(target, dataAccessId, pmoPath, params,
 				rs -> new OpcoesPeriodoPpaLoaDto(null, rs.get("ppa").asText())).get(0);
-		
+
 	}
 
-	public List<OpcoesPpaLoaDto> AnosPpaAtivo() {
+	public List<OpcoesPpaLoaDto> listarAnosPpaAtivo() {
+
 		Map<String, Object> params = Map.of();
 
 		String pmoPath = siscapSigefesPath;
-		String target = targetPlanejamentoPpa;
-		String dataAccessId = planejamentoDataAccessId;
+		String target = targetAnosPpa;
+		String dataAccessId = anosPpaDataAccessId;
 
 		return apiUtils.consult(target, dataAccessId, pmoPath, params,
-				rs -> new OpcoesPeriodoPpaLoaDto(null, rs.get("ppa").asText())).get(0);
+				rs -> new OpcoesPpaLoaDto(
+						rs.get("ano").asLong(),
+						rs.get("ano").asText()));
+
 	}
 
-	// public List<OpcoesIndicadoresDto> listarIndicadoresFiltro(
-	// 		Long filtroGestao,
-	// 		FiltroIndicadorDto filtro) {
-
-	// 	if (filtroGestao == null) {
-	// 		throw new SiscapServiceException(Arrays.asList("Gestão é obrigatória"));
-	// 	}
-
-	// 	List<IndicadorPentahoBiDto> listaIndicadoresBI = this.listarIndicadoresBI(filtro);
-
-	// 	String idsIndicadores = listaIndicadoresBI.stream()
-	// 			.map(ind -> String.valueOf(ind.idIndicador()))
-	// 			.distinct()
-	// 			.collect(Collectors.joining(","));
-
-	// 	List<OdsIndicadorExternoDto> listaOdsBI = this.listarOdsPorIndicadoresBI(idsIndicadores);
-
-	// 	Map<Integer, List<OdsIndicadorExternoDto>> odsPorIndicador = listaOdsBI.stream()
-	// 			.collect(Collectors.groupingBy(
-	// 					OdsIndicadorExternoDto::IndicadorId,
-	// 					Collectors.mapping(ods -> new OdsIndicadorExternoDto(
-	// 							ods.IndicadorId(),
-	// 							ods.odsId(),
-	// 							ods.odsOrdem(),
-	// 							ods.odsNome(),
-	// 							ods.odsDescricao(),
-	// 							obterCorOds(ods.odsOrdem())), Collectors.toList())));
-
-	// 	return listaIndicadoresBI.stream()
-	// 			.collect(Collectors.groupingBy(IndicadorPentahoBiDto::idIndicador))
-	// 			.values()
-	// 			.stream()
-	// 			.map(grupo -> {
-
-	// 				IndicadorPentahoBiDto primeiro = grupo.get(0);
-
-	// 				Set<String> chavesMetas = new HashSet<>();
-
-	// 				List<MetasIndicadorExternoDto> metas = grupo.stream()
-	// 						.filter(item -> item.anoMeta() != null || item.valorMeta() != null)
-	// 						.map(item -> new MetasIndicadorExternoDto(
-	// 								null,
-	// 								item.anoMeta(),
-	// 								item.valorMeta() != null
-	// 										? toBigDecimal(item.valorMeta())
-	// 										: null))
-	// 						.filter(meta -> chavesMetas.add(meta.anoMeta() + "|" + meta.valorMeta()))
-	// 						.toList();
-
-	// 				List<OdsIndicadorExternoDto> ods = odsPorIndicador.getOrDefault(
-	// 						primeiro.idIndicador(),
-	// 						Collections.emptyList());
-
-	// 				return new OpcoesIndicadoresDto(
-	// 						primeiro.idIndicador(),
-	// 						primeiro.nomeIndicador(),
-	// 						primeiro.unidadeMedida(),
-	// 						primeiro.polaridade(),
-	// 						primeiro.medidoPor(),
-	// 						metas,
-	// 						primeiro.maiorAnoIndicador(),
-	// 						primeiro.maiorMetaIndicador() != null
-	// 								? BigDecimal.valueOf(primeiro.maiorMetaIndicador())
-	// 								: null,
-	// 						ods);
-	// 			})
-	// 			.toList();
-
-	// }
-
-	// private List<OdsIndicadorExternoDto> listarOdsPorIndicadoresBI(String idsIndicadores) {
-
-	// 	String parametroIndicadores = idsIndicadores != null && !idsIndicadores.trim().isEmpty()
-	// 			? idsIndicadores.trim()
-	// 			: "-1";
-
-	// 	Map<String, Object> params = Map.of(
-	// 			"paramp_ods", "-1",
-	// 			"paramp_indicadores", parametroIndicadores);
-
-	// 	return apiUtils.consult(targetOdsIndicadores, odsIndicadoresDataAccessId, siscapPath, params, rs -> {
-
-	// 		return new OdsIndicadorExternoDto(
-	// 				rs.get("IndicadorId").asInt(),
-	// 				rs.get("OdsId").asInt(),
-	// 				rs.get("ordemOds").asInt(),
-	// 				rs.get("nomeOds").asText(),
-	// 				rs.get("DescricaoOds").asText(),
-	// 				obterCorOds(rs.get("ordemOds").asInt()));
-	// 	});
-
-	// }
-
-	// public List<OpcoesIndicadoresDto> cadastrarIndicadorAvulso(IndicadorAvulsoForm form) {
-
-	// 	if (form == null) {
-	// 		throw new SiscapServiceException(Arrays.asList("Dados do indicador são obrigatórios"));
-	// 	}
-
-	// 	return Collections.emptyList();
-
-	// }
-
-	// public List<GestaoPentahoBiDto> listarGestoesAtivasBI() {
+	public List<OpcoesPpaLoaDto> listarUosAnoPpa(Long ano) {
 
-	// 	String ativa = "1"; // gestoes ativas
-
-	// 	Map<String, Object> params = Map.of(
-	// 			"paramp_ativa", ativa);
-
-	// 	String pmoPath = siscapPath;
-	// 	String target = targetGestao;
-	// 	String dataAccessId = gestaoDataAccessId;
-
-	// 	return apiUtils.consult(target, dataAccessId, pmoPath, params,
-	// 			rs -> new GestaoPentahoBiDto(
-	// 					rs.get("idGestao").asInt(),
-	// 					rs.get("ativa").asInt(),
-	// 					rs.get("nomeGestao").asText(),
-	// 					rs.get("descricaoGestao").asText(),
-	// 					rs.get("deAno").asInt(),
-	// 					rs.get("ateAno").asInt(),
-	// 					rs.get("modelNameGestao").asText()));
-
-	// }
-
-	// public List<DesafiosPentahoBiDto> listarDesafiosGestaoBI() {
-
-	// 	String ativa = "1"; // todos
-
-	// 	Map<String, Object> params = Map.of(
-	// 			"paramp_ativa", ativa,
-	// 			"paramp_idGestao", "-1");
-
-	// 	String pmoPath = siscapPath;
-	// 	String target = targetDesafios;
-	// 	String dataAccessId = desafiosDataAccessId;
+		Map<String, Object> params = Map.of(
+				"paramp_ano", ano);
 
-	// 	return apiUtils.consult(target, dataAccessId, pmoPath, params,
-	// 			rs -> new DesafiosPentahoBiDto(
-	// 					rs.get("gestaoId").asInt(),
-	// 					rs.get("desafioId").asInt(),
-	// 					rs.get("desafio").asText(),
-	// 					rs.get("grupoId").asInt(),
-	// 					rs.get("subGrupoId").asInt()));
-
-	// }
+		String pmoPath = siscapSigefesPath;
+		String target = targetUosPpa;
+		String dataAccessId = uosPpaDataAccessId;
 
-	// public List<IndicadorPentahoBiDto> listarIndicadoresBI(FiltroIndicadorDto filtro) {
+		return apiUtils.consult(target, dataAccessId, pmoPath, params,
+				rs -> new OpcoesPpaLoaDto(
+						rs.get("cod_uo").asLong(),
+						rs.get("mne_uo").asText()));
 
-	// 	String desafios = "-1"; // todos
-	// 	String organizadores = "-1"; // todos
-	// 	String indicadores = "-1"; // todos
+	}
 
-	// 	if (filtro != null) {
+	public List<OpcoesPpaLoaDto> listarFuncoes(List<Long> anos, List<Long> uos) {
 
-	// 		if (filtro.desafios() != null && !filtro.desafios().isEmpty()) {
-	// 			desafios = montarListaIds(filtro.desafios());
-	// 		}
+		String anosFormatados = anos.stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining(","));
 
-	// 		if (filtro.labels() != null && !filtro.labels().isEmpty()) {
-	// 			organizadores = montarOrganizadores(filtro.labels());
-	// 		}
+		String uosFormatados = uos.stream()
+				.map(uo -> String.format("%05d", uo))
+				.collect(Collectors.joining(","));
 
-	// 		if (filtro.indicadores() != null && !filtro.indicadores().isEmpty()) {
-	// 			indicadores = montarIndicadores(filtro.indicadores());
-	// 		}
+		Map<String, Object> params = Map.of(
+				"paramp_ano", anosFormatados,
+				"paramp_cod_uo", uosFormatados);
 
-	// 	}
+		String pmoPath = siscapSigefesPath;
+		String target = targetFuncoesPpa;
+		String dataAccessId = funcoesPpaDataAccessId;
 
-	// 	Map<String, Object> params = Map.of(
-	// 			"paramp_desafio", desafios,
-	// 			"paramp_organizador", organizadores,
-	// 			"paramp_indicador", indicadores);
+		return apiUtils.consult(target, dataAccessId, pmoPath, params,
+				rs -> new OpcoesPpaLoaDto(
+						rs.get("cod_funcao").asLong(),
+						rs.get("nom_funcao").asText()));
 
-	// 	String pmoPath = siscapPath;
-	// 	String target = targetIndicadores;
-	// 	String dataAccessId = indicadoresDataAccessId;
+	}
 
-	// 	return apiUtils.consult(target, dataAccessId, pmoPath, params,
-	// 			rs -> new IndicadorPentahoBiDto(
-	// 					rs.get("ativa").asInt(),
-	// 					rs.get("idGestao").asInt(),
-	// 					rs.get("nomeGestao").asText(),
-	// 					rs.get("modelNameGestao").asText(),
-	// 					rs.get("idDesafio").asInt(),
-	// 					rs.get("nomeDesafio").asText(),
-	// 					rs.get("idOrganizador").asInt(),
-	// 					rs.get("nomeOrganizador").asText(),
-	// 					rs.get("modelNameOrganizador").asText(),
-	// 					rs.get("idIndicador").asInt(),
-	// 					rs.get("nomeIndicador").asText(),
-	// 					rs.get("unidadeMedida").asText(),
-	// 					rs.get("polaridade").asText(),
-	// 					rs.get("medidoPor").asText(),
-	// 					rs.get("anoMeta").asInt(),
-	// 					rs.get("valorMeta").asText(),
-	// 					rs.get("maiorAnoIndicador").asInt(),
-	// 					rs.get("maiorMetaIndicador").asDouble()));
-
-	// }
-
-	// private String montarIndicadores(List<Integer> idIndicadores) {
-
-	// 	if (idIndicadores == null || idIndicadores.isEmpty()) {
-	// 		return "-1";
-	// 	}
-
-	// 	return idIndicadores.stream()
-	// 			.distinct()
-	// 			.map(String::valueOf)
-	// 			.collect(Collectors.joining(","));
-
-	// }
-
-	// private String montarOrganizadores(List<FiltroLabelDto> labels) {
-
-	// 	String organizadores = labels == null
-	// 			? "-1"
-	// 			: labels.stream()
-	// 					.filter(label -> label.idLabelValores() != null)
-	// 					.flatMap(label -> label.idLabelValores().stream())
-	// 					.distinct()
-	// 					.map(String::valueOf)
-	// 					.collect(Collectors.joining(","));
-
-	// 	if (organizadores.isBlank()) {
-	// 		organizadores = "-1";
-	// 	}
-
-	// 	return organizadores;
-
-	// }
-
-	// private String montarListaIds(List<Long> ids) {
-
-	// 	if (ids == null || ids.isEmpty()) {
-	// 		return "-1";
-	// 	}
-
-	// 	return ids.stream()
-	// 			.distinct()
-	// 			.map(String::valueOf)
-	// 			.collect(Collectors.joining(","));
-	// }
-
-	// public List<OdsPentahoBiDto> listarOdsBI() {
-
-	// 	Map<String, Object> params = Map.of();
-
-	// 	return apiUtils.consult(targetOds, odsDataAccessId, siscapPath, params,
-	// 			rs -> new OdsPentahoBiDto(
-	// 					rs.get("OdsId").asInt(),
-	// 					rs.get("DescricaoOds").asText(),
-	// 					rs.get("nomeOds").asText(),
-	// 					rs.get("ordemOds").asInt(),
-	// 					obterCorOds(rs.get("ordemOds").asInt())));
-
-	// }
-
-	// private BigDecimal toBigDecimal(String valor) {
-
-	// 	if (valor == null || valor.isBlank()) {
-	// 		return null;
-	// 	}
-
-	// 	valor = valor.trim();
-
-	// 	if (valor.equalsIgnoreCase("null")
-	// 			|| valor.equalsIgnoreCase("nan")
-	// 			|| valor.equalsIgnoreCase("n/d")) {
-	// 		return null;
-	// 	}
-
-	// 	return new BigDecimal(valor.replace(",", "."));
-	// }
-
-	// private String obterCorOds(Integer ordemOds) {
-
-	// 	if (ordemOds == null) {
-	// 		return null;
-	// 	}
-
-	// 	return switch (ordemOds) {
-	// 		case 1 -> "#E5243B";
-	// 		case 2 -> "#DDA63A";
-	// 		case 3 -> "#4C9F38";
-	// 		case 4 -> "#C5192D";
-	// 		case 5 -> "#FF3A21";
-	// 		case 6 -> "#26BDE2";
-	// 		case 7 -> "#FCC30B";
-	// 		case 8 -> "#A21942";
-	// 		case 9 -> "#FD6925";
-	// 		case 10 -> "#DD1367";
-	// 		case 11 -> "#FD9D24";
-	// 		case 12 -> "#BF8B2E";
-	// 		case 13 -> "#3F7E44";
-	// 		case 14 -> "#0A97D9";
-	// 		case 15 -> "#56C02B";
-	// 		case 16 -> "#00689D";
-	// 		case 17 -> "#19486A";
-	// 		case 18 -> "#7A3A1A";
-	// 		default -> null;
-	// 	};
-
-	// }
-
-	// public List<LabelDTO> montarLabelsGrupoSubgrupo(List<OrganizadorGestaoPentahoBiDto> listaBi) {
-
-	// 	Map<String, Map<Integer, LabelValorDTO>> labelsMap = new LinkedHashMap<>();
-
-	// 	for (OrganizadorGestaoPentahoBiDto item : listaBi) {
-
-	// 		adicionarValorLabel(
-	// 				labelsMap,
-	// 				item.labelGrupo(),
-	// 				item.idGrupo(),
-	// 				item.valorGrupo(),
-	// 				null);
-
-	// 		adicionarValorLabel(
-	// 				labelsMap,
-	// 				item.labelSubGrupo(),
-	// 				item.idSubGrupo(),
-	// 				item.valorSubGrupo(),
-	// 				item.idGrupo());
-
-	// 	}
-
-	// 	return labelsMap.entrySet().stream()
-	// 			.map(entry -> new LabelDTO(
-	// 					entry.getKey().hashCode(),
-	// 					entry.getKey(),
-	// 					null,
-	// 					new ArrayList<>(entry.getValue().values())))
-	// 			.toList();
-
-	// }
-
-	// private void adicionarValorLabel(
-	// 		Map<String, Map<Integer, LabelValorDTO>> labelsMap,
-	// 		String nomeLabel,
-	// 		Integer idValor,
-	// 		String valorLabel,
-	// 		Integer idPai) {
-
-	// 	if (nomeLabel == null || idValor == null || valorLabel == null) {
-	// 		return;
-	// 	}
-
-	// 	labelsMap
-	// 			.computeIfAbsent(nomeLabel.trim(), key -> new LinkedHashMap<>())
-	// 			.putIfAbsent(
-	// 					idValor,
-	// 					new LabelValorDTO(
-	// 							idValor,
-	// 							valorLabel.trim(),
-	// 							idPai));
-	// }
-
-	// private List<OrganizadorGestaoPentahoBiDto> listarOrganizadoresGestaoBI(Integer idGestao) {
-
-	// 	Map<String, Object> params = Map.of(
-	// 			"paramp_idGestao", idGestao != null ? String.valueOf(idGestao) : "-1");
-
-	// 	return apiUtils.consult(targetOrganizacao, organizacaoDataAccessId, siscapPath, params,
-	// 			rs -> new OrganizadorGestaoPentahoBiDto(
-	// 					rs.get("gestaoId").asInt(),
-	// 					rs.get("idGrupo").asInt(),
-	// 					rs.get("labelGrupo").asText(),
-	// 					rs.get("descricaoGrupo").asText(),
-	// 					rs.get("idSubGrupo").asInt(),
-	// 					rs.get("labelSubGrupo").asText(),
-	// 					rs.get("descricaoSubGrupo").asText()));
-
-	// }
-
-	// // public List<IndicadorPentahoBiDto> listarIndicadoresBIPorId(List<Integer>
-	// // idsIndicadoresBI) {
-	// // Map<String, Object> params = Map.of(
-	// // "paramp_idindicadores", idsIndicadoresBI);
-	// // String pmoPath = siscapPath;
-	// // String target = targetIndicadores;
-	// // String dataAccessId = indicadoresDataAccessId;
-	// // return apiUtils.consult(target, dataAccessId, pmoPath, params,
-	// // rs -> new IndicadorPentahoBiDto(
-	// // rs.get("ativa").asInt(),
-	// // rs.get("idGestao").asInt(),
-	// // rs.get("nomeGestao").asText(),
-	// // rs.get("modelNameGestao").asText(),
-	// // rs.get("idDesafio").asInt(),
-	// // rs.get("nomeDesafio").asText(),
-	// // rs.get("idOrganizador").asInt(),
-	// // rs.get("nomeOrganizador").asText(),
-	// // rs.get("modelNameOrganizador").asText(),
-	// // rs.get("idIndicador").asInt(),
-	// // rs.get("nomeIndicador").asText(),
-	// // rs.get("unidadeMedida").asText(),
-	// // rs.get("polaridade").asText(),
-	// // rs.get("medidoPor").asText(),
-	// // rs.get("anoMeta").asInt(),
-	// // rs.get("valorMeta").asText(),
-	// // rs.get("maiorAnoIndicador").asInt(),
-	// // rs.get("maiorMetaIndicador").asDouble()));
-	// // }
+	public List<OpcoesPpaLoaDto> listarProgramas(List<Long> anos, List<Long> uos, List<Long> funcoes) {
+
+		String anosFormatados = anos.stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining(","));
+
+		String uosFormatados = uos.stream()
+				.map(uo -> String.format("%05d", uo))
+				.collect(Collectors.joining(","));
+
+		String funcoesFormatadas = funcoes.stream()
+				.map(funcao -> String.format("%01d", funcao))
+				.collect(Collectors.joining(","));
+
+		Map<String, Object> params = Map.of(
+				"paramp_ano", anosFormatados,
+				"paramp_cod_uo", uosFormatados,
+				"paramp_cod_funcao", funcoesFormatadas);
+
+		String pmoPath = siscapSigefesPath;
+		String target = targetProgramasPpa;
+		String dataAccessId = programasPpaDataAccessId;
+
+		return apiUtils.consult(target, dataAccessId, pmoPath, params,
+				rs -> new OpcoesPpaLoaDto(
+						rs.get("cod_programa").asLong(),
+						rs.get("nom_programa").asText()));
+
+	}
+
+	public List<OpcoesPpaLoaDto> listarAcoes(List<Long> funcoes, List<Long> programas, List<Long> anos,
+			List<Long> uos) {
+
+		String anosFormatados = anos.stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining(","));
+
+		String uosFormatados = uos.stream()
+				.map(uo -> String.format("%05d", uo))
+				.collect(Collectors.joining(","));
+
+		String funcoesFormatadas = funcoes.stream()
+				.map(funcao -> String.format("%05d", funcao))
+				.collect(Collectors.joining(","));
+
+		String programasFormatados = programas.stream()
+				.map(programa -> String.format("%05d", programa))
+				.collect(Collectors.joining(","));
+
+		Map<String, Object> params = Map.of(
+				"paramp_ano", anosFormatados,
+				"paramp_cod_uo", uosFormatados,
+				"paramp_cod_funcao", funcoesFormatadas,
+				"paramp_cod_programa", programasFormatados);
+
+		String pmoPath = siscapSigefesPath;
+		String target = targetAcoesPpa;
+		String dataAccessId = acoesPpaDataAccessId;
+
+		return apiUtils.consult(target, dataAccessId, pmoPath, params,
+				rs -> new OpcoesPpaLoaDto(
+						rs.get("cod_acao").asLong(),
+						rs.get("nom_acao").asText()));
+
+	}
 
 }
