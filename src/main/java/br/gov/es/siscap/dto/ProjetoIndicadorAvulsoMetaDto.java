7397f52a41bd8506a5039f4ed3e60239eb0a9a53
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2026-08-12 20:26:16 -0300

    commit ajustes limite campos valor meta indicadores

--- src/main/java/br/gov/es/siscap/dto/ProjetoIndicadorAvulsoMetaDto.java
+++ src/main/java/br/gov/es/siscap/dto/ProjetoIndicadorAvulsoMetaDto.java
@@ -2,10 +2,12 @@ package br.gov.es.siscap.dto;
 
 import br.gov.es.siscap.models.IndicadorAvulsoMeta;
 import br.gov.es.siscap.models.ProjetoIndicadorAvulsoMeta;
+import jakarta.validation.constraints.Size;
 
 public record ProjetoIndicadorAvulsoMetaDto(
 		Integer id,
 		Integer anoMeta,
+		@Size(max = 20, message = "O valor da meta do indicador de projeto deve possuir no máximo 20 caracteres." )
 		String valorMeta) {
 
 	public ProjetoIndicadorAvulsoMetaDto(IndicadorAvulsoMeta meta) {
--- src/main/java/br/gov/es/siscap/dto/ProjetoIndicadorCatalogoMetaDto.java
+++ src/main/java/br/gov/es/siscap/dto/ProjetoIndicadorCatalogoMetaDto.java
@@ -1,10 +1,12 @@
 package br.gov.es.siscap.dto;
 
 import br.gov.es.siscap.models.ProjetoIndicadorExternoMeta;
+import jakarta.validation.constraints.Size;
 
 public record ProjetoIndicadorCatalogoMetaDto(
 		Integer id,
 		Integer anoMeta,
+		@Size(max = 20, message = "O valor da meta de indicador do projeto deve possuir no máximo 20 caracteres." )
 		String valorMeta) {
 
 	public ProjetoIndicadorCatalogoMetaDto(ProjetoIndicadorExternoMeta meta) {
