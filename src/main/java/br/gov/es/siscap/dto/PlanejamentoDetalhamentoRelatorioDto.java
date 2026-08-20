31052ddf8e81a03ede6df7de263db24316543da2
Eduardo Simonato <eduardo.simonato@pdcase.com.br>
2026-08-19 21:42:45 -0300

    ajustes montagem dados planejamento no PDF do DIC

--- src/main/java/br/gov/es/siscap/dto/PlanejamentoDetalhamentoRelatorioDto.java
+++ src/main/java/br/gov/es/siscap/dto/PlanejamentoDetalhamentoRelatorioDto.java
@@ -3,10 +3,30 @@ package br.gov.es.siscap.dto;
 import java.math.BigDecimal;
 
 public record PlanejamentoDetalhamentoRelatorioDto(
-    String gnd,
-    String modalidade,
-    String idUso,
-    String fonte,
-    BigDecimal valor ) {
+        String gnd,
+        String modalidade,
+        String idUso,
+        String fonte,
+        BigDecimal valor) {
+
+    public String getGnd() {
+        return gnd;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
+
+    public String getIdUso() {
+        return idUso;
+    }
+
+    public String getFonte() {
+        return fonte;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
 
 }
