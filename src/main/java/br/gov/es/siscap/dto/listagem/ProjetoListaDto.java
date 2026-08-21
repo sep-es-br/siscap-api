34df4481096164c9833df57e2e04352979f2fbf0
Eduardo Simonato <ersimonato.es@gmail.com>
2026-08-21 12:18:52 -0300

    atualizar dto da lista de projetos acrescentando data de criação

--- src/main/java/br/gov/es/siscap/dto/listagem/ProjetoListaDto.java
+++ src/main/java/br/gov/es/siscap/dto/listagem/ProjetoListaDto.java
@@ -3,29 +3,32 @@ package br.gov.es.siscap.dto.listagem;
 import br.gov.es.siscap.models.Projeto;
 import br.gov.es.siscap.models.StatusProjeto;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public record ProjetoListaDto(
 
-			Long id,
-			String sigla,
-			String titulo,
-			String status,
-			BigDecimal valorEstimado,
-			boolean isRascunho,
-			String protocoloEdocs,
-			Long lotacaoUsuario
+		Long id,
+		String sigla,
+		String titulo,
+		String status,
+		BigDecimal valorEstimado,
+		boolean isRascunho,
+		String protocoloEdocs,
+		Long lotacaoUsuario,
+		LocalDateTime dataCriacaoDic
+
 ) {
-	public ProjetoListaDto( Projeto projeto, BigDecimal valorEstimado, Long lotacaoUsuario ) {
+	public ProjetoListaDto(Projeto projeto, BigDecimal valorEstimado, Long lotacaoUsuario) {
 		this(
-			projeto.getId(),
-			projeto.getSigla(),
-			projeto.getTitulo(),
-                        Optional.ofNullable(projeto.getStatusAtual()).map(StatusProjeto::getStatus).orElse(null),
-			valorEstimado,
-			projeto.isRascunho(),
-			projeto.getProtocoloEdocs(),
-			lotacaoUsuario
-		);
+				projeto.getId(),
+				projeto.getSigla(),
+				projeto.getTitulo(),
+				Optional.ofNullable(projeto.getStatusAtual()).map(StatusProjeto::getStatus).orElse(null),
+				valorEstimado,
+				projeto.isRascunho(),
+				projeto.getProtocoloEdocs(),
+				lotacaoUsuario,
+				projeto.getCriadoEm());
 	}
 }
